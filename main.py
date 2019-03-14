cad8af324aad13569e89c3c613d7c0f672fb295a
nrhint <nathan@hintonclan.org>
2019-03-13 20:45:52 -0600

    downloaded and extracted resources.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #Nathan Hinton
 #This will be an ide game. hopefuly...
+
+#Starting with a tutuorial from online:
+#https://www.raywenderlich.com/2795-beginning-game-programming-for-teens-with-python
+import pygame
+from pygame.locals import *
+
+pygame.init()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
